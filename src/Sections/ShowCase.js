44d5990e2533fa88122f2bf6393bd2050cf04f1e
Tobias Larm <tobbe-l@outlook.com>
2022-11-07 21:02:58 +0100

    fixed showcase

--- src/Sections/ShowCase.js
+++ src/Sections/ShowCase.js
@@ -6,14 +6,18 @@ import PersonInCoat from '../Assets/person-in-coat.svg'
 const ShowCase = () => {
   return (
     <section className="showcase">
-        <div className="container flex c-center">
-            <img className="img-left" src={GirlOnChair} alt="Girl-on-chair" />
+        <div className="container flex">
+            <div>
+              <img className="img-left" src={GirlOnChair} alt="Girl-on-chair" />
+            </div>
             <div className="showcase-text">
                 <h1>SALE UP<span>To 50% Off</span></h1>
                 <p>Online shopping free home delivery over $100</p>
                 <ButtonTheme text="SHOP NOW"/>
             </div>
-            <img className="img-right" src={PersonInCoat} alt="Person-in-a-coat" />
+            <div>
+              <img className="img-right" src={PersonInCoat} alt="Person-in-a-coat" />
+            </div>
         </div>
     </section>
   )
